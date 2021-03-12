48b7781bfff1008869dfad2d63e274246a25b44c
munirrani <munir@tuahpacket.net>
2021-03-12 21:25:43 +0800

    Alternative solution for Q14 Lab 1

--- Week 1/Lab Answers/q14.py
+++ Week 1/Lab Answers/q14.py
@@ -4,3 +4,7 @@ words = sentence.split()
 reversed_words = list(reversed(words))
 for words in reversed_words:
     print(words,end=' ')
+
+# Alternative solution
+sentence = [word for word in input('Enter a sentence :').split()]
+print(' '.join(sentence[::-1]))
